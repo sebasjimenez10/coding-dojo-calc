a9fbf74af0dbce72a377c8ef3851d1db8079b858
Sebastian Jimenez V <sjimen14@eafit.edu.co>
2013-08-26 17:02:24 -0500

    Paso 2: Agrego el minimo codigo posible para que la prueba pase

--- src/codigo/Calculadora.java
+++ src/codigo/Calculadora.java
@@ -4,7 +4,7 @@ public class Calculadora {
 
 	public int realizarOperacion(int operando1, int operando2, String operador) {
 		//Escribir el minimo codigo posible para que la prueba pase.
-		return 0;
+		return 6;
 	}
 
 }
